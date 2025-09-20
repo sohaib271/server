039ddcf1be59c995c4f8c6d005fa11ef9dd5691e
sohaib271 <sohaibsheikh6299@gmail.com>
2025-09-20 14:29:52 +0500

    Prisma resolved

--- dist/model/prisma.d.ts
+++ dist/model/prisma.d.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
-declare const prisma: PrismaClient<import("../generated/prisma").Prisma.PrismaClientOptions, never, import("../generated/prisma/runtime/library").DefaultArgs>;
+import { PrismaClient } from "@prisma/client";
+declare const prisma: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
 export default prisma;
 //# sourceMappingURL=prisma.d.ts.map
